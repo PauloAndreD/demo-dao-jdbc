54c12157cc3c6f98fd2aa8780ae2017e21d5b36e
PauloAndreD <pauloandre.dv@gmail.com>
2023-12-06 10:36:22 -0300

    DepartmentDao and SellerDao interfaces

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,2 +1,14 @@
-package model.dao;public interface DepartmentDao {
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Department obj);
+
+    Department findById(Integer id);
+    List<Department> findAll();
 }
